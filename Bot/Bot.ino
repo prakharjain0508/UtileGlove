085d76bbc838f89d3c0c1833cf80d41f69b1f1e5
prakharjain0508 <45605417+prakharjain0508@users.noreply.github.com>
2021-03-03 14:28:18 -0600

    Add files via upload

---
+++ Bot/Bot.ino
@@ -0,0 +1,146 @@
+char buffer[9];
+const int buffer_len = 9;
+boolean characterSent = false;
+boolean leftRight = false;
+int accel = 0;
+int turn = 0;
+int reverse = 0;
+const int flex = A0;
+const int force = A1;
+const byte D1 = 5;
+const byte D2 = 3;
+const byte D3 = 10;
+const byte D4 = 9;
+int FlexValue=0;
+int ForceValue = 255;
+int FlexValueMax = 100;
+int FlexValueMin = 0;
+int FlexValueMid = 50;
+int FlexValueMidUpper = FlexValueMid - 10; 
+
+//DC motor variables
+byte motorSpeed = 0;
+byte motorSpeedMax = 255;
+byte motorSpeedMin = 0; //set to smallest value that make motor move (default 0)
+                         // DC motor that I use start to move at 90 pwm value
+
+
+void clearBuffer()
+{
+  for(int i = 0; i < buffer_len; i++)
+  {
+    buffer[i] = 0;
+  }
+}
+
+
+void setup() 
+{
+ 
+ pinMode(flex, INPUT);
+ pinMode(force, INPUT);
+ pinMode(D1, OUTPUT);
+ pinMode(D2, OUTPUT);
+ pinMode(D3, OUTPUT);
+ pinMode(D4, OUTPUT);
+ Serial.begin(9600);
+ clearBuffer();
+ Serial.flush();
+}
+
+void loop() 
+{
+  if(!characterSent)
+  {
+    Serial.print('a'); 
+    characterSent = true;
+  }
+  
+  //Get commands
+  if(Serial.available())
+  {
+    characterSent = false;
+    char check = Serial.read();
+  
+    if(check == '.')
+    { 
+      for (int i=0; i <buffer_len; i++)
+      {
+        buffer[i] = Serial.read();
+        delay(10);
+      }
+      
+      char acceleration[3] =  { buffer[0], buffer[1], buffer[2] };
+      char reverseAccel[3] =  { buffer[3], buffer[4], buffer[5] };
+      char steering[3] = { buffer[6], buffer[7], buffer[8] };
+      accel        = 100*(acceleration[0] - '0') + 10*(acceleration[1]-'0') + (acceleration[2]-'0');
+      reverse      = 100*(reverseAccel[0] - '0') + 10*(reverseAccel[1]-'0') + (reverseAccel[2]-'0');
+      turn         = 100*(steering[0] - '0') + 10*(steering[1]-'0') + (steering[2]-'0');
+      if(accel > FlexValueMidUpper && reverse < 50) //Forward
+    {
+        motorSpeed = map(accel, FlexValueMax, FlexValueMidUpper, motorSpeedMin, motorSpeedMax);
+        MotorForward(motorSpeed);
+    }
+    
+    else if(accel > FlexValueMidUpper && reverse > 50) //Backward
+    {
+       motorSpeed = map(accel, FlexValueMidUpper, FlexValueMax, motorSpeedMin, motorSpeedMax);
+       MotorBackward(motorSpeed);
+    }
+    else if(turn > 178) //Right
+    {
+        motorSpeed = map(turn, 255, 178, motorSpeedMin, motorSpeedMax);
+        MotorRight(motorSpeed);
+    }
+    else if(turn < 100)  //Left
+    {
+        motorSpeed = map(turn, 78, 0, motorSpeedMin, motorSpeedMax);
+        MotorLeft(motorSpeed);
+    }
+    else 
+    {
+       MotorStop(); 
+    }
+  }
+ }
+   
+}
+ void MotorForward( byte spd)
+{
+    digitalWrite(D2, HIGH);
+    analogWrite(D1, spd);
+    digitalWrite(D3, HIGH);
+    analogWrite(D4, spd);
+   
+}
+void MotorBackward( byte SpdY)
+{
+    digitalWrite(D2, LOW);
+    analogWrite(D1, SpdY); 
+    digitalWrite(D3, LOW);
+    analogWrite(D4, SpdY);
+}
+void MotorLeft( byte spdL)
+{
+    digitalWrite(D2, HIGH);
+    analogWrite(D1, spdL);
+    digitalWrite(D4, HIGH);
+    analogWrite(D3, spdL);
+}
+
+void MotorRight( byte SpdR)
+{
+    digitalWrite(D2, LOW);
+    analogWrite(D1, SpdR); 
+    digitalWrite(D4, LOW);
+    analogWrite(D3, SpdR);
+}
+void MotorStop()
+{
+    analogWrite(D1, LOW);   
+    digitalWrite(D2, LOW);
+    analogWrite(D3, LOW);   
+    digitalWrite(D4, LOW);
+}
+
+    
---
+++ Glove/Glove.ino
@@ -0,0 +1,221 @@
+const int right_flex = A0;
+const int right_force  = A1;
+int rfi;
+int rfr;
+int rgy;
+int rfi_straight = 775;
+int rfi_curled = 1023;
+const int rfr_low      = 1023; 
+const int rfr_high     = 0;
+int rgy_mid;
+      int rgy_low;
+      int rgy_high;
+
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+	#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define INTERRUPT_PIN 2  
+#define LED_PIN 13 
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() 
+{
+    mpuInterrupt = true;
+}
+
+void calibrate_gyro(int Gyro_avg)
+{
+ int sumy = 0;
+ for (int i = 0; i<20; i++)
+  {
+    sumy = sumy + (Gyro_avg);
+  }
+  delay(100);
+  double avgy = round(sumy/20);
+  rgy_mid = avgy;
+  rgy_low = 0;
+  rgy_high = avgy*2;
+}
+void print_pretty(int val)
+{
+  int new_val;
+  //All Numbers must range from zero to 255.
+  if      (val<0)   new_val = 0;
+  else if (val>255) new_val = 255;
+  else    new_val = val;
+  //Add Leading zeros
+  if (new_val < 10)
+  {
+    Serial.print(0);
+    Serial.print(0);
+    Serial.print(new_val);
+  }
+  else if (new_val >= 10 && new_val <100)
+  {
+    Serial.print(0);
+    Serial.print(new_val);
+  }
+  else Serial.print(new_val);
+}
+
+void setup() 
+{
+   pinMode(right_flex, INPUT);
+   pinMode (right_force,  INPUT);
+   Serial.begin(9600);
+   
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+    #endif
+    Serial1.begin(9600);
+    while (!Serial1); // wait for Leonardo enumeration, others continue immediately
+    // initialize device
+    Serial1.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+    
+    // verify connection
+    Serial1.println(F("Testing device connections..."));
+    Serial1.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial1.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) 
+    {
+        // turn on the DMP, now that it's ready
+        Serial1.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial1.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial1.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } 
+    else 
+    {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial1.print(F("DMP Initialization failed (code "));
+        Serial1.print(devStatus);
+        Serial1.println(F(")"));
+    }
+    calibrate_gyro (((ypr[2] * 180/M_PI) + 90));
+}
+
+
+void loop() 
+{
+  // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        //wait here
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial1.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+         //   Serial.print("roll\t");
+           // Serial.println((ypr[2] * 180/M_PI)+90);
+           // delay(10);
+        #endif
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
+
+  
+  rfi = analogRead(right_flex);
+  rgy = ((ypr[2] * 180/M_PI)+90);
+  int sumr = 0;
+  for (int i=0; i<10; i++)
+  {
+    sumr = sumr + analogRead(right_force);   //Right Force Ring
+  }
+  rfr = round(sumr/10);  //Right Force Ring
+  
+  int RFI = map(rfi, rfi_curled, rfi_straight, 255, 0);
+  int RFR = map(rfr, rfr_low,      rfr_high,   0, 255);
+  int RGY = map(rgy, 0,  180,   0, 255);
+  Serial.print('.');
+  print_pretty(RFI);
+  print_pretty(RFR);
+  print_pretty(RGY);
+  delay(10);
+  while (Serial.read() != 'a');
+  
+}
